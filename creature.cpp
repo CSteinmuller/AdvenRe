6914294a944fbc4176b521e5939185809c3950e2
CSteinmuller <chris.dungeonmaster@gmail.com>
2017-04-25 15:33:30 -0500

    begining of oop
    rudimentary start of making code object oriented

---
+++ creature.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<fstream>
+#include<cstring>
+#include<string>
+
+using namespace std;
+
+class creature
+{
+private:
+int hp;
+int chp;
+string wep;
+int damage;
+string spell;
+int sdamage;
+int xp;
+int gp;
+};
